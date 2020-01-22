39537f660daeb502a8b5c0161e04498f1a423d0c
SamuraiViking <nelson67@stolaf.edu>
2020-01-22 10:10:24 -0600

    totally working

--- src/static/BackendUrl.js
+++ src/static/BackendUrl.js
@@ -1,7 +1,6 @@
 
 // TESTING URL
-// export default "https://pos-backend-test.herokuapp.com/"
-
+export default "https://pos-backend-test.herokuapp.com/"
 
 // PRODUCTION URL
-export default "https://prepnetwork-pos-backend.herokuapp.com/"
+// export default "https://prepnetwork-pos-backend.herokuapp.com/"
